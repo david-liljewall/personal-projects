123a3aca6bde844d5006a5764f0bfd5e71d84cb0
david-liljewall <davidliljewall96@gmail.com>
2020-08-26 16:32:06 -0500

    edits

--- email-sms/send-file-email.py
+++ email-sms/send-file-email.py
@@ -1,13 +1,15 @@
+
 import smtplib
 from email.message import EmailMessage
 
 
 def email_alert( subject, body, to ):
     
+    # Set email message variables:
     msg  = EmailMessage()
-    msg.set_content( body )
     msg[ 'subject' ] = subject
     msg[ 'to' ] = to
+    msg.set_content( body )
     
     
     # use existing gmail account
@@ -15,17 +17,24 @@ def email_alert( subject, body, to ):
     password = "qmptnzejmrkbjndd" # provided Google app password (UNIQUE FOR THIS PROJECT)
     msg[ 'from' ] = user
     
-    # login to server, use TLS inspection, login, then send desired message
-    server = smtplib.SMTP( "smtp.gmail.com", 587 )
-    server.starttls()
+    # # login to server, use TLS inspection, login, then send desired message
+    # server = smtplib.SMTP( "smtp.gmail.com", 587 )
+    # server.starttls()
+    # server.login( user, password )
+    # server.send_message( msg )
+    
+    server = smtplib.SMTP_SSL( 'smpt.gmail.com', 465 )
     server.login( user, password )
     server.send_message( msg )
     
+    
+    # quit server
     server.quit()
     
+
     
-    
-#* If main program running, run the code above. Won't run if imported
+# ---- If main program running, run the code above. Won't run if imported ---- #
+
 if __name__=='__main__':
     
     
@@ -36,8 +45,9 @@ if __name__=='__main__':
     print( "Enter the subject line:" )
     sub = input()
     
-    print( "Enter the Email body text:" )
-    bod = input()
+    with open( 'Mars.jpg', 'rb' ) as file:
+        file_data = file.read()
+
     
     # call variables and send email
-    email_alert( sub, bod, recipient )
+    email_alert( sub, "See attached file" , recipient )
---
+++ email-sms/send-text-email.py
@@ -0,0 +1,51 @@
+import smtplib
+from email.message import EmailMessage
+
+
+def email_alert( subject, body, to ):
+    
+    # Set email message variables:
+    msg  = EmailMessage()
+    msg[ 'subject' ] = subject
+    msg[ 'to' ] = to
+    msg.set_content( body )
+    
+    
+    # use existing gmail account
+    user = "davidliljewall96@gmail.com"
+    password = "qmptnzejmrkbjndd" # provided Google app password (UNIQUE FOR THIS PROJECT)
+    msg[ 'from' ] = user
+    
+    # # login to server, use TLS inspection, login, then send desired message
+    # server = smtplib.SMTP( "smtp.gmail.com", 587 )
+    # server.starttls()
+    # server.login( user, password )
+    # server.send_message( msg )
+    
+    server = smtplib.SMTP_SSL( 'smpt.gmail.com', 465 )
+    server.login( user, password )
+    server.send_message( msg )
+    
+    
+    # quit server
+    server.quit()
+    
+
+    
+# ---- If main program running, run the code above. Won't run if imported ---- #
+
+if __name__=='__main__':
+    
+    
+    # create variables for email sending
+    print( "Enter the desired recipient:" )
+    recipient = input()
+    
+    print( "Enter the subject line:" )
+    sub = input()
+    
+    print( "Enter the Email body text:" )
+    bod = input()
+    
+    # call variables and send email
+    email_alert( sub, bod, recipient )
